16f64074233a876b41d0d46c5e5c274d352ea1b6
Bahrudin Hrnjica <bhrnjica@BAHR-PC01>
2018-02-11 19:36:17 +0100

    First commit

---
+++ Nokia3310_detection.py
@@ -0,0 +1,52 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root
+# for full license information.
+# ==============================================================================
+import os, sys
+import numpy as np
+import utils.od_utils as od
+from utils.config_helpers import merge_configs
+
+def get_configuration():
+    # load configs for detector, base network and data set
+    from FasterRCNN.FasterRCNN_config import cfg as detector_cfg
+
+    # for VGG16 base model use:         from utils.configs.VGG16_config import cfg as network_cfg
+    # for AlexNet base model use:       from utils.configs.AlexNet_config import cfg as network_cfg
+    from utils.configs.AlexNet_config import cfg as network_cfg
+    # for Pascal VOC 2007 data set use: from utils.configs.Pascal_config import cfg as dataset_cfg
+    # for the Grocery data set use:     from utils.configs.Grocery_config import cfg as dataset_cfg
+    # from utils.configs.Grocery_config import cfg as dataset_cfg
+    ###################################################################
+    # Custom Dataset NO3310
+    from utils.configs.NO3310_config import cfg as dataset_cfg
+
+    return merge_configs([detector_cfg, network_cfg, dataset_cfg, {'DETECTOR': 'FasterRCNN'}])
+
+if __name__ == '__main__':
+    # Get Configuration
+    cfg = get_configuration()
+
+    # train and test
+    eval_model = od.train_object_detector(cfg)
+    eval_results = od.evaluate_test_set(eval_model, cfg)
+
+    # write AP results to output
+    for class_name in eval_results: print('AP for {:>15} = {:.4f}'.format(class_name, eval_results[class_name]))
+    print('Mean AP = {:.4f}'.format(np.nanmean(list(eval_results.values()))))
+
+    # detect objects in single image
+    img_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), r"DataSets/NO3310/testImages/img30.jpg")
+    regressed_rois, cls_probs = od.evaluate_single_image(eval_model, img_path, cfg)
+    bboxes, labels, scores = od.filter_results(regressed_rois, cls_probs, cfg)
+
+    # write detection results to output
+    fg_boxes = np.where(labels > 0)
+    print("#bboxes: before nms: {}, after nms: {}, foreground: {}".format(len(regressed_rois), len(bboxes), len(fg_boxes[0])))
+    for i in fg_boxes[0]: print("{:<12} (label: {:<2}), score: {:.3f}, box: {}".format(
+                                cfg["DATA"].CLASSES[labels[i]], labels[i], scores[i], [int(v) for v in bboxes[i]]))
+    # visualize detections on image
+    od.visualize_results(img_path, bboxes, labels, scores, cfg)
+    # measure inference time
+   # od.measure_inference_time(eval_model, img_path, cfg, num_repetitions=100)
---
+++ README.md
@@ -0,0 +1,48 @@
+Detecting Nokia3310 mobile phone using CNTK and Python
+
+## Overview
+
+This example is modified version of predefined CNTK Example of Image detection which can be found at http://github.com/Microsoft/CNTK.
+
+By running `Nokia3310_detection.py` the code will do the following:
+
+* Train and create CNTK based model whih can detect NOkia3310 on an image.
+* By providing test images the model can be evaluated and tested.
+* The example uses minimal python code, needed to run object detection by using FasterRCNN.
+* The model is not trained from zero, it is based on AlexNet pre trained model.
+
+
+## Running the example
+
+### Setup
+
+To run Nokia3310 object detection example you need a CNTK 2.3, Python 3.5 environment. Beside the basics requiremens you need to install the following additional packages:
+
+```
+pip install opencv-python easydict pyyaml future
+```
+
+The also code uses prebuild Cython modules for parts of the region proposal network (see `utils/cython_modules`). 
+
+If you want to use the debug output you need to run `pip install pydot_ng` ([website](https://pypi.python.org/pypi/pydot-ng)) and install [graphviz](http://graphviz.org/) to be able to plot the CNTK graphs (the GraphViz executable has to be in the system’s PATH).
+
+### Getting the data and AlexNet model
+
+The example uses the pre-trained AlexNet model which can be downloaded by running the following Python command from the PretrainedModels folder:
+
+`python download_model.py`
+
+### Running the demo
+
+To train and evaluate a detector run
+
+`python Nokia3310_detection.py`
+
+#### Changing the data set
+
+In order to change DataSet you have to provide the images and data. More information about how to prepare image and data can be found at http://bhrnjica.net  
+
+
+#### Changing the base model
+
+The does not support changing the base model. 
